ccc9d31ffa2a0040c983b7d3bf5ef9ca22a49a52
gasidet2501 <s6406021622141@email.kmutnb.ac.th>
2023-02-23 16:21:45 +0700

    update

--- src/CRUDBookSQLite.js
+++ src/CRUDBookSQLite.js
@@ -63,12 +63,12 @@ app.post('/books', (req, res) => {
 app.put('/books/:id', (req, res) => {
     const book = req.body;
     db.run('UPDATE books SET title = ?, author = ? WHERE id = ?', book.title, book.author, req.params.id, function(err) {
-    if (err) {
-        res.status(500).send(err);
-    }
-    else{
-        res.send (book);
-    }
+        if (err) {
+            res.status(500).send(err);
+        }
+        else{
+            res.send (book);
+        }
     });
 });
 
